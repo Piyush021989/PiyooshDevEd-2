5a8b69aa24a622af46d2be4af662cfb3881a5c0d
Piyush020989 <35917725+Piyush021989@users.noreply.github.com>
2023-08-27 23:01:46 +0200

    LWCPromptTag

---
+++ force-app/main/default/lwc/lightningPromptExample/lightningPromptExample.js
@@ -0,0 +1,18 @@
+import { LightningElement } from 'lwc';
+import LightningPrompt from 'lightning/prompt';
+
+
+export default class LightningPromptExample extends LightningElement {
+    handlePromptClick() {
+        LightningPrompt.open({
+            message: 'this is the prompt message',
+            //theme defaults to "default"
+            label: 'Please Respond', // this is the header text
+            defaultValue: 'initial input value', //this is optional
+        }).then((result) => {
+            //Prompt has been closed
+            //result is input text if OK clicked
+            //and null if cancel was clicked
+        });
+    }
+}
